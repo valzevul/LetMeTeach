ac71d19e3360338ce6a28a24d716d590f520dd87
Vadim Drobinin <vadim@drobinin.com>
2014-10-14 15:27:47 +0400

    Initial commit from local repo

---
+++ LetMeTeach/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LetMeTeach
+//
+//  Created by Vadim Drobinin on 14/10/14.
+//  Copyright (c) 2014 Vadim Drobinin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
