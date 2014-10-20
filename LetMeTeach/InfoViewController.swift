706b9e76b9e967f0d17af248600400bdbad28f54
Vadim Drobinin <vadim@drobinin.com>
2014-10-20 13:41:26 +0400

    Added "good" way to change screens.

--- LetMeTeach/InfoViewController.swift
+++ LetMeTeach/InfoViewController.swift
@@ -21,13 +21,13 @@ class InfoViewController: UIViewController {
 
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
     override func viewWillDisappear(animated: Bool) {
         navigationController?.navigationBarHidden = true
     }
+    
+    @IBAction func closeButtonAction(sender: UIBarButtonItem) {
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
 }
 
